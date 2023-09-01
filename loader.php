27864e7f872becd17949a565eb7b57c30cd97f63
tirtharajkarbotkar1cci <143815674+tirtharajkarbotkar1cci@users.noreply.github.com>
2023-09-01 19:15:36 +0530

    Add files via upload

--- loader.php
+++ loader.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Initialize the plugin.
+ * Load legacy files.
  */
 
 if ( ! function_exists( 'cky_define_constants' ) ) {
@@ -10,25 +10,43 @@ if ( ! function_exists( 'cky_define_constants' ) ) {
 	 * @return void
 	 */
 	function cky_define_constants() {
-		if ( ! defined( 'CKY_PLUGIN_URL' ) ) {
-			define( 'CKY_PLUGIN_URL', plugin_dir_url( __FILE__ ) );
+		if ( ! defined( 'CLI_PLUGIN_URL' ) ) {
+			define( 'CLI_PLUGIN_URL', plugin_dir_url( __FILE__ ) );
 		}
-		if ( ! defined( 'CKY_APP_ASSETS_URL' ) ) {
-			define( 'CKY_APP_ASSETS_URL', plugin_dir_url( __FILE__ ) . 'frontend/images/' );
+		if ( ! defined( 'CLI_PLUGIN_PATH' ) ) {
+			define( 'CLI_PLUGIN_PATH', plugin_dir_path( __FILE__ ) );
 		}
 	}
 }
 
+/**
+ * Function to activate the plugin.
+ *
+ * @return void
+ */
+function cky_activate() {
+	Cookie_Law_Info_Activator::activate();
+}
+
+/**
+ * The code that runs during plugin deactivation.
+ * This action is documented in includes/class-cookie-law-info-deactivator.php
+ */
+function cky_deactivate() {
+	require_once plugin_dir_path( __FILE__ ) . 'includes/class-cookie-law-info-deactivator.php';
+	Cookie_Law_Info_Deactivator::deactivate();
+}
+
 cky_define_constants();
+require_once plugin_dir_path( __FILE__ ) . 'includes/class-cookie-law-info-activator.php';
+register_activation_hook( __FILE__, 'cky_activate' );
+register_deactivation_hook( __FILE__, 'cky_deactivate' );
 
-require_once CLI_PLUGIN_BASEPATH . 'class-autoloader.php';
+/**
+ * The core plugin class that is used to define internationalization,
+ * admin-specific hooks, and public-facing site hooks.
+ */
+require plugin_dir_path( __FILE__ ) . 'includes/class-cookie-law-info.php';
 
-$autoloader = new \CookieYes\Lite\Autoloader();
-$autoloader->register();
-
-register_activation_hook( __FILE__, array( \CookieYes\Lite\Includes\Activator::get_instance(), 'install' ) );
-
-$cky_loader = new \CookieYes\Lite\Includes\CLI();
+$cky_loader = new Cookie_Law_Info();
 $cky_loader->run();
-
-
